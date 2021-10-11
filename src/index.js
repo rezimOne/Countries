c663b9ec41298bd69f496df0d24e4fd013c8e22d
rezimOne <michalreda@gmail.com>
2021-10-11 10:54:05 +0200

    feature (task3 function)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const timeKey = "time";
 const countryKey = "countries";
 const currTime = new Date().getTime();
 const prevTime = parseInt(localStorage.getItem(timeKey));
-const interval = 10000;
+const interval = 10000000;
 const citizensSum = 500000000;
 
 const compareData = (currData, prevData) => {
@@ -65,7 +65,7 @@ const fetchData = async () => {
 }
 
 
-if(!localStorage.getItem(countryKey) || prevTime + interval  <= currTime){
+if(!localStorage.getItem(countryKey) || prevTime + interval  >= currTime){
   fetchData().then(response => {
     const fetchedData = response;
     localStorage.setItem(timeKey, JSON.stringify(currTime));
@@ -77,6 +77,51 @@ if(!localStorage.getItem(countryKey) || prevTime + interval  <= currTime){
       compareData(fetchedData, storedData);
       euCountries(storedData);
       populationSumCheck(storedData);
+      task3(storedData);
     }
+
   });
+}
+//zadanie 3
+
+const task3 = (el) => {
+
+  const myCountries = {
+      EU: {countries: [], population: 0, languages: {}, currencies: []},
+      AU: {countries: [], population: 0, languages: {}, currencies: []},
+      NAFTA: {countries: [], population: 0, languages: {}, currencies: []},
+      other: {countries: [], population: 0, languages: {}, currencies: []}
+  };
+
+  for(let i = 0; i < el.length; i++){
+    if(el[i].regionalBlocs){
+      for(let j = 0; j < el[i].regionalBlocs.length; j++){
+        if(el[i].regionalBlocs[j].acronym === "EU"){
+          myCountries.EU.countries.push(el[i].nativeName);
+          myCountries.EU.currencies.push(el[i].currencies[0].code);
+          myCountries.EU.population += el[i].population;
+        }else if(el[i].regionalBlocs[j].acronym === "AU"){
+          myCountries.AU.countries.push(el[i].nativeName);
+          myCountries.AU.currencies.push(el[i].currencies[0].code);
+          myCountries.AU.population += el[i].population;
+        }else if(el[i].regionalBlocs[j].acronym === "NAFTA"){
+          myCountries.NAFTA.countries.push(el[i].nativeName);
+          myCountries.NAFTA.currencies.push(el[i].currencies[0].code);
+          myCountries.NAFTA.population += el[i].population;
+        }else{
+          myCountries.other.countries.push(el[i].nativeName);
+        }
+      }
+    }else{
+      myCountries.other.countries.push(el[i].nativeName);
+    }
+  }
+
+  myCountries.EU.currencies = [...new Set(myCountries.EU.currencies)];
+  myCountries.AU.currencies = [...new Set(myCountries.AU.currencies)];
+  myCountries.NAFTA.currencies = [...new Set(myCountries.NAFTA.currencies)];
+  myCountries.other.currencies = [...new Set(myCountries.other.currencies)];
+
+  console.log('myCountries:', myCountries)
+
 }
